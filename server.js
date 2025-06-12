c7251642707137e7ddcd9826bb0021369276331c
DhafinAghniya <148194082+DhafinAghniya@users.noreply.github.com>
2025-06-12 21:50:18 +0700

    perbaikan server.js

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.use('/api', createProxyMiddleware({
+  target: 'https://back-end-acu-ai-production.up.railway.app',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', 
+  },
+}));
+
+app.use('/predict', createProxyMiddleware({
+  target: 'https://dhapunk-integrasi-acuai.hf.space',
+  changeOrigin: true,
+}));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
